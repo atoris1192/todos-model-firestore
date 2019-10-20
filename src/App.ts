fd3bc21f8433c199bc29bbd19bfa05f926f288a5
atoris1192 <atoris1192@gmail.com>
2019-10-20 19:27:13 +0900

    App.ts整理

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,7 @@
 import { render, element } from "./view/html-util";
-import  firebase from 'firebase';
+// import  firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const config = {
   apiKey: "AIzaSyCd5tM9yxxuy_JaB7dq5ZDi_WkuB6g9FNs",
@@ -16,21 +18,6 @@ const db = firebase.firestore()
 const collection = db.collection('todos')
 
 
-const todos = [
-  { id: 100, title: "title100", isDone: false },
-  { id: 101, title: "title101", isDone: true },
-  { id: 102, title: "title102", isDone: false },
-]
-class TodoItemModel {
-  private id: number;
-  private title: string;
-  private isDone: boolean;
-  constructor({ title }) {
-    this.id = Date.now();
-    this.title = title;
-    this.isDone = false
-  }
-}
 class TodoListModel {
   private items: any;
   constructor(items = []) {
@@ -45,8 +32,14 @@ class TodoListModel {
   getTotalCount() {
     return this.items.length;
   }
-  addTodo( item ) {
-    this.items.push( item )
+  addTodo({ title }) {
+    collection.add({
+      id: Date.now(),
+      title: title,
+      isDone: false,
+      created_at: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    // this.items.push( item )
   }
 }
 export class App {
@@ -55,56 +48,42 @@ export class App {
     this.todoListModel = new TodoListModel();
   }
     
-  render() {
-    const jsTodoList = document.querySelector('#js-todo-list');
-    const jsTodoCount = document.querySelector('#js-todo-count');
-
-    const ul = element`<ul />` ;
-    const items = this.todoListModel.getItems();
-    const itemTotalCount = this.todoListModel.getTotalCount();
-
-    items.forEach( item => {
-      const li = element`<li>${ item.title }</li>`
-      ul.appendChild(li);
-    })
-    jsTodoCount.textContent = `Total Items Count: ${ itemTotalCount }`
-    render(ul, jsTodoList)
-  }
-  firstRender() {
-    this.todoListModel.setItems(todos);
-    this.render()
-  }
   addTodo() {
     const jsForm = document.querySelector('#js-form');
     const jsFormInput: any = document.querySelector('#js-form-input');
 
     jsForm.addEventListener('submit', (event) =>{
       event.preventDefault();
-      this.todoListModel.addTodo( new TodoItemModel({
-        title: jsFormInput.value 
-      }))
-
-
-    collection.add({
-      id: Date.now(),
-      title: jsFormInput.value,
-      isDone: false,
-      created_at: firebase.firestore.FieldValue.serverTimestamp()
-    })
-
-
+      this.todoListModel.addTodo({
+        title: jsFormInput.value
+      })
 
       jsFormInput.value = '';
       // this.render();
     })
   }
+  test01() { // all items
+    let citiesRef = db.collection('todos');
+    let allCities = citiesRef.get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          console.log(doc.id, '=>', doc.data());
+        });
+      })
+      .catch(err => {
+        console.log('Error getting documents', err);
+      });
+  }
 
   main() {
     const jsTodoList = document.querySelector('#js-todo-list');
+    const jsTodoCount = document.querySelector('#js-todo-count');
     const ul = document.createElement('ul');
+    
+  //  this.test01() 
+    
 
     this.addTodo();
-    // this.firstRender();
 
     collection.orderBy('created_at', 'desc')
       .onSnapshot( snapshot => {
@@ -118,6 +97,5 @@ export class App {
       })
 
     render(ul, jsTodoList);
-
   }
 }
